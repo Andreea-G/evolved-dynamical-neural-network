debd6c57f90aba628df5b188676bb9b53c597caa
Garrett_R <garrettreynolds5@gmail.com>
2014-07-14 04:11:45 -0700

    Created Python png to maze file converter

---
+++ maze_files/png_to_maze.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 14 03:25:28 2014
+
+@author: garrett
+"""
+
+import matplotlib.image as mpimg
+import argparse
+import numpy as np
+
+
+##This snippet allows you to have new lines in the program description below
+class MyParser( argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write( 'error: {}\n'.format(message) )
+        self.print_help()
+        sys.exit(2)
+
+def main():
+
+	parser = argparse.ArgumentParser(description="Converts a .png into a maze file for input to pinn"
+						"\nNote: the image should be made of 4 types pixels in RGB:"
+						"\n(0,0,0) -> 0"
+						"\n(x,0,0) -> 1 (where x is non-zero, for example, it could be 255)"
+						"\n(0,x,0) -> 2"
+						"\n(0,0,x) -> 3"
+						"\n\nExample usage: ./png_to_maze -i ~/my_maze.png -o ./maze1)")
+	parser.add_argument('-i', '--input', type=str, required=True, help="input image filename")		
+	parser.add_argument('-o', '--output', type=str, required=True, help="input image filename")
+	args = parser.parse_args()	
+	
+	image_filename = args.input
+	output_filename = args.output	
+	
+	img = mpimg.imread(image_filename)
+	
+	#turn 3-channel image array into 1-channel array
+			
+	with open(output_filename, 'w') as file:
+		for row in range(img.shape[0]):
+			for col in range(img.shape[1]):
+				current_pix = img[row][col][:]
+				#if the pixel is RGB=(0,0,0), then set maze to be zero for this point
+				if np.where(current_pix!=0)[0].size == 0:
+					maze_value = 0
+				else:
+					#set maze value to be N if the N-th element of (R,G,B) != 0
+					maze_value = np.where(current_pix!=0)[0][0] + 1
+				file.write(str(maze_value) + ' ')
+			file.write('\n')
+	
+
+
+
+if __name__ == '__main__':
+	main()
