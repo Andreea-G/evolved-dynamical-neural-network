b35ba9930b16af3c06f108989fb22f0fd83c1600
Garrett_R <garrettreynolds5@gmail.com>
2014-08-20 22:47:11 -0700

    Added documentation

--- run_pinn.sh
+++ run_pinn.sh
@@ -1,32 +1,64 @@
 #!/usr/bin/env bash
 
+######################################################################
+############User-defined parameters (please edit these to your liking)
+######################################################################
+
+#Number of brains in each generation
 num_brains=15
+#Number of neurons in each brain
 num_neurons=500
+#verage activation threshold for each neruon
 av_active_threshold=0.25
+#Standard deviation for activation threshold for each neruon
 st_dev_active_threshold=0.04
+#Start activation for each neruon (at beginning of maze)
 start_activation=0.25
+#Average decay rate for each neruon
 av_decay_rate=0.001
+#Standard deviation for decay rate for each neruon
 st_dev_decay_rate=0.0003
+#Average number of synapses for each neruon
 av_num_syn=10
+#Standard deviation of number of synapses for each neruon
 st_dev_num_syn=2
+#Average synapse strength for each neruon
 av_syn_strength=0.05
+#Standard deviation of synapse strength for each neruon
 st_dev_syn_strength=0.04
+#Maximum allowed decision before a brain receives worst fitness score
 max_decisions=100
+#Duration of input signal in cycles
 input_duration=10
+#Number of cycles between start of input and start of output
 input_output_delay=5
+#Duration of output signal in cycles
 output_duration=10
+#Duration between end of output and start of next input in cycles
 deadtime_duration=0
-maze_map_file=maze_files/test_maze4
+#The maze filepath, see docs for expected file format
+maze_map_file=maze_files/example_maze
+#Have players start at random points in maze (you should probably not use this)
 maze_random_start=false
+#Number of generations
 num_generations=10
+#Number of mutated neurons each time a brain is mutated
 num_mutated_neurons=1
+#Number of mutated synapses in each mutated neuron
 num_mutated_synapses=1
+#Probability of each brain coming from asexual reproduction
 prob_asexual=1
+#Mutate the decay rate everytime a neuron gets mutated
 mutate_decay_rate=false
+#Mutate the active threshold everytime a neuron gets mutated
 mutate_active_threshold=false
+#Max number of threads to use
 max_num_threads=8
 
+
+############################################################################################################
 ############################################## Don't edit beyond here ######################################
+############################################################################################################
 
 #Prepare flags
 flags=""
