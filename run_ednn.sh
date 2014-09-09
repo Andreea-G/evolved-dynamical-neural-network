3b93b351e0854d2afbaa0c5d0ef83dd809e927ad
Garrett_R <garrettreynolds5@gmail.com>
2014-09-09 14:36:15 +0100

    Renamed project
    Renamed to 'ednn' standing for Evolved Dynamical Neural Net

--- run_ednn.sh
+++ run_ednn.sh
@@ -7,7 +7,7 @@
 #Number of brains in each generation
 num_brains=15
 #Number of neurons in each brain
-num_neurons=500
+num_neurons=5000
 #verage activation threshold for each neruon
 av_active_threshold=0.25
 #Standard deviation for activation threshold for each neruon
@@ -15,7 +15,7 @@ st_dev_active_threshold=0.04
 #Start activation for each neruon (at beginning of maze)
 start_activation=0.25
 #Average decay rate for each neruon
-av_decay_rate=0.001
+av_decay_rate=0.02
 #Standard deviation for decay rate for each neruon
 st_dev_decay_rate=0.0003
 #Average number of synapses for each neruon
@@ -72,9 +72,9 @@ if [ "$maze_active_threshol" == "true" ]; then
     flags="${flags}-V "
 fi
 
-echo -e "Command to execute is:\n\n   ./pinn  $flags  --num_brains ${num_brains} --num_neurons $num_neurons --av_active_threshold $av_active_threshold --st_dev_active_threshold $st_dev_active_threshold --start_activation $start_activation --av_decay_rate $av_decay_rate --st_dev_decay_rate $st_dev_decay_rate --av_num_syn $av_num_syn --st_dev_num_syn $st_dev_num_syn --av_syn_strength $av_syn_strength --st_dev_syn_strength $st_dev_syn_strength --max_decisions $max_decisions --input_duration $input_duration --input_output_delay $input_output_delay --output_duration $output_duration --deadtime_duration $deadtime_duration --maze_map_file $maze_map_file --num_generations $num_generations --num_mutated_neurons $num_mutated_neurons --num_mutated_synapses $num_mutated_synapses --prob_asexual $prob_asexual --max_num_threads $max_num_threads"
+echo -e "Command to execute is:\n\n   ./ednn  $flags  --num_brains ${num_brains} --num_neurons $num_neurons --av_active_threshold $av_active_threshold --st_dev_active_threshold $st_dev_active_threshold --start_activation $start_activation --av_decay_rate $av_decay_rate --st_dev_decay_rate $st_dev_decay_rate --av_num_syn $av_num_syn --st_dev_num_syn $st_dev_num_syn --av_syn_strength $av_syn_strength --st_dev_syn_strength $st_dev_syn_strength --max_decisions $max_decisions --input_duration $input_duration --input_output_delay $input_output_delay --output_duration $output_duration --deadtime_duration $deadtime_duration --maze_map_file $maze_map_file --num_generations $num_generations --num_mutated_neurons $num_mutated_neurons --num_mutated_synapses $num_mutated_synapses --prob_asexual $prob_asexual --max_num_threads $max_num_threads"
 
 echo -e "\n\n____________________________________________"
 
-./pinn  $flags  --num_brains ${num_brains} --num_neurons $num_neurons --av_active_threshold $av_active_threshold --st_dev_active_threshold $st_dev_active_threshold --start_activation $start_activation --av_decay_rate $av_decay_rate --st_dev_decay_rate $st_dev_decay_rate --av_num_syn $av_num_syn --st_dev_num_syn $st_dev_num_syn --av_syn_strength $av_syn_strength --st_dev_syn_strength $st_dev_syn_strength --max_decisions $max_decisions --input_duration $input_duration --input_output_delay $input_output_delay --output_duration $output_duration --deadtime_duration $deadtime_duration --maze_map_file $maze_map_file --num_generations $num_generations --num_mutated_neurons $num_mutated_neurons --num_mutated_synapses $num_mutated_synapses --prob_asexual $prob_asexual --max_num_threads $max_num_threads
+./ednn  $flags  --num_brains ${num_brains} --num_neurons $num_neurons --av_active_threshold $av_active_threshold --st_dev_active_threshold $st_dev_active_threshold --start_activation $start_activation --av_decay_rate $av_decay_rate --st_dev_decay_rate $st_dev_decay_rate --av_num_syn $av_num_syn --st_dev_num_syn $st_dev_num_syn --av_syn_strength $av_syn_strength --st_dev_syn_strength $st_dev_syn_strength --max_decisions $max_decisions --input_duration $input_duration --input_output_delay $input_output_delay --output_duration $output_duration --deadtime_duration $deadtime_duration --maze_map_file $maze_map_file --num_generations $num_generations --num_mutated_neurons $num_mutated_neurons --num_mutated_synapses $num_mutated_synapses --prob_asexual $prob_asexual --max_num_threads $max_num_threads
 
